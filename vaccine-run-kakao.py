fc13e336c1c88c01ad5e2b71ac3696ac183dc1ac
SJang1 <git@sjang.dev>
2021-07-21 17:04:49 +0900

    small fix

--- vaccine-run-kakao.py
+++ vaccine-run-kakao.py
@@ -9,10 +9,10 @@ import datetime
 urllib3.disable_warnings()
 
 print("사각형 모양으로 백신범위를 지정한 뒤, 해당 범위 안에 있는 백신을 조회해서 남은 백신이 있으면 Chrome 브라우저를 엽니다.")
-topx = str(input("사각형의 위쪽 좌측 x값을 넣어주세요 : "))
-topy = str(input("사각형의 위쪽 좌측 y값을 넣어주세요 : "))
-botx = str(input("사각형의 아래쪽 우측 x값을 넣어주세요 : "))
-boty = str(input("사각형의 아래쪽 우측 y값을 넣어주세요 : "))
+topx = str(input("사각형의 위쪽 좌측 x값을 넣어주세요. 127.xxxxxx : "))
+topy = str(input("사각형의 위쪽 좌측 y값을 넣어주세요 37.xxxxx : "))
+botx = str(input("사각형의 아래쪽 우측 x값을 넣어주세요 127.xxxxx : "))
+boty = str(input("사각형의 아래쪽 우측 y값을 넣어주세요 37.xxxxx: "))
 APIURL = 'https://vaccine-map.kakao.com/api/v2/vaccine/left_count_by_coords'
 APIdata = '{"bottomRight":{"x":' + botx + ',"y":' + boty + '},"onlyLeft":false,"order":"latitude","topLeft":{"x":' + topx + ',"y":' + topy + '}}'
 print(APIdata)
@@ -60,7 +60,6 @@ orgCdCode = str(x.get('orgCode'))
 latkey = str(x.get('y'))
 lngkey = str(x.get('x'))
 
-time.sleep(0.13)
 os.system('/usr/bin/open -a "/Applications/Google Chrome.app" "https://vaccine.kakao.com/reservation/' + orgCdCode
  + '?from=https%3A%2F%2Fvaccine.kakao.com%2Fdetail%2F' + orgCdCode + '%3Flat%3D' + latkey + '%26lng%3D' + lngkey + '%26from%3DMap%26closable=false'
  + '&code=VEN00013"')
