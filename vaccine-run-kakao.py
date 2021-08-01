83af7f915c9741fffd8bd64c6810b320760f8567
MPThLee <mpthlee@gmail.com>
2021-08-01 17:53:38 +0900

    feat: check default cookie is exist

--- vaccine-run-kakao.py
+++ vaccine-run-kakao.py
@@ -13,6 +13,7 @@ import telepot
 import unicodedata
 import urllib3
 import re
+import platform
 
 search_time = 0.2  # 잔여백신을 해당 시간마다 한번씩 검색합니다. 단위: 초
 urllib3.disable_warnings()
@@ -80,10 +81,35 @@ def load_cookie_config():
             return None
     return None
 
-# 쿠키를 global jar 함수에 로드함.
+# cookie 경로가 입력되지 않았을시, 쿠키 파일이 Default 경로에 있는지 확인함
+# 경로가 입력되었거나, Default 경로의 쿠키가 존재해야 global jar 함수에 cookie를 로드함.
 def load_cookie():
     global jar
-    jar = browser_cookie3.chrome(cookie_file=load_cookie_config(), domain_name=".kakao.com")
+
+    cookie_file = load_cookie_config()
+    cookie_path = None
+
+    os_type = platform.system()
+    if os_type == "Linux":
+        # browser_cookie3 also checks beta version of google chrome's cookie file.
+        cookie_path = os.path.expanduser("~/.config/google-chrome/Default/Cookies")
+        if os.path.exists(cookie_path) is False:
+            cookie_path = os.path.expanduser("~/.config/google-chrome-beta/Default/Cookies")
+    elif os_type == "Darwin":
+        cookie_path = os.path.expanduser("~/Library/Application Support/Google/Chrome/Default/Cookies")
+    elif os_type == "Windows":
+        cookie_path = os.path.expandvars("%LOCALAPPDATA%/Google/Chrome/User Data/Default/Cookies")
+    else: #Jython?
+        print("지원하지 않는 환경입니다.")
+        close()
+
+    if cookie_file is None and os.path.exists(cookie_path) is False:
+        print("기본 쿠키 파일 경로에 파일이 존재하지 않습니다. 아래 링크를 참조하여 쿠키 파일 경로를 지정해주세요.\n" +
+              "https://github.com/SJang1/korea-covid-19-remaining-vaccine-macro/discussions/403")
+        close()
+
+    jar = browser_cookie3.chrome(cookie_file=cookie_file, domain_name=".kakao.com")
+
 
 def check_user_info_loaded():
     user_info_api = 'https://vaccine.kakao.com/api/v1/user'
